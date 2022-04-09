0a5554b9e127ecd54e428817017375ca4f4cebc2
MladenLm <mladenlamesevic@gmail.com>
2022-04-09 18:33:51 +0200

    finished projets

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
 const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menuItem = document.querySelectorAll('.header .nav-bar .nav-list ul a');
 const header = document.querySelector('.header.container')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active')
     mobile_menu.classList.toggle('active')
+})
+
+document.addEventListener('scroll', () => {
+    let scroll_position = window.scrollY;
+
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c'
+    } else {
+        header.style.backgroundColor = 'transparent'
+    }
+})
+
+menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
 })
